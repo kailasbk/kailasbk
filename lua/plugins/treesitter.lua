d0e602aefd88b0f8606183101e23e428a3c79f42
Kailas B. Kahler <kailasbk230@gmail.com>
2025-02-04 22:19:29 -0500

    Add treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,1 +1,14 @@
-return {}
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    opts = {
+      highlight = {
+        enable = true,
+      },
+    },
+    config = function(_, opts)
+      require('nvim-treesitter.configs').setup(opts)
+    end,
+  }
+}
